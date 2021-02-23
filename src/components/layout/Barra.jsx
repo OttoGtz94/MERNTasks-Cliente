1b458e4adf304500beb2c61667815b7a3cf1e5f1
Otto Gutierrez <ogutierrezgz.94@hotmail.com>
2021-02-23 14:17:17 -0600

    Poner el nombre del usuario autenticado en el header

--- src/components/layout/Barra.jsx
+++ src/components/layout/Barra.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import AuthContext from '../../context/autenticacion/authContext';
 
 const Barra = () => {
+	// extraer la inf de autenticacion
+	const authContext = useContext(AuthContext);
+	const { usuario, usuarioAutenticado } = authContext;
+
+	useEffect(() => {
+		usuarioAutenticado();
+	}, []);
 	return (
 		<header className='app-header'>
-			<p className='nombre-usuario'>
-				Hola <span>Otto</span>
-			</p>
+			{usuario ? (
+				<p className='nombre-usuario'>
+					Hola <span>{usuario.nombre}</span>
+				</p>
+			) : null}
+
 			<nav className='nav-principal'>
 				<a href='#!'>Cerrar sesión</a>
 			</nav>
